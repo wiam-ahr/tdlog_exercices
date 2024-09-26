aef5f16ce49c5a65d10fb6d04b5089b994c3ee54
Ahrabar Wiam <wiam.ahrabar@centrale-casablanca.ma>
2024-09-26 12:11:27 +0100

    Update exo1.py

--- exo1/exo1.py
+++ exo1/exo1.py
@@ -7,3 +7,7 @@ A new item can be created with Item(price, weight)
     Write the code for this class, with the appropriate constructor.
 Example of code using the class: i = Item(10, 20)
 """
+class Item:
+    def __init__(self, price, weight):
+        self.price = price
+        self.weight = weight
--- exo1/test_exo1.py
+++ exo1/test_exo1.py
@@ -9,3 +9,17 @@ class Exo1Test(unittest.TestCase):
         item = Item(10, 20)
 
         self.assertEqual(20, item.weight)
+
+# test_exo1.py
+import unittest
+from exo1 import Item
+
+class Exo1Test(unittest.TestCase):
+
+    def test_item_construction(self):
+        item = Item(10, 20)
+        self.assertEqual(item.price, 10)
+        self.assertEqual(item.weight, 20)
+
+if __name__ == '__main__':
+    unittest.main()
